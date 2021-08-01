2d84f8f5a7ef9467dfd7557f9d655dc0224a1557
alexcho617 <alexcho617@gmail.com>
2021-08-02 00:00:35 +0900

    back up

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'screens/login_route.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -11,26 +10,24 @@ void main() async {
   runApp(
     MaterialApp(
       theme: ThemeData(
-      primaryColor: Color(0xFF89A1F8),
-      iconTheme: IconThemeData(
-        color: Color(0xFF89A1F8)
-      ),
-      outlinedButtonTheme: OutlinedButtonThemeData(
-        style: OutlinedButton.styleFrom(
-          side: BorderSide(color: const Color(0xff89A1F8)),
-          primary: const Color(0xff89A1F8),
+        primaryColor: Color(0xFF89A1F8),
+        iconTheme: IconThemeData(color: Color(0xFF89A1F8)),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            side: BorderSide(color: const Color(0xff89A1F8)),
+            primary: const Color(0xff89A1F8),
+          ),
         ),
-      ),
-      appBarTheme: AppBarTheme(
-        backgroundColor: Colors.white,
-      ),
-      textTheme: TextTheme(
-        bodyText2: TextStyle(
-          fontFamily: 'Rubik',
-          fontSize: 16,
-          letterSpacing: 1,
-          color: Color(0xFF89A1F8),
-
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+        ),
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            fontFamily: 'Rubik',
+            fontSize: 16,
+            letterSpacing: 1,
+            color: Color(0xFF89A1F8),
+          ),
         ),
       ),
       title: 'DoItChurch Navigation',
