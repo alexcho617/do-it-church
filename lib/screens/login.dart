4edfc14eda1be82f8e380c61e6bd32c7a59b3299
alexcho617 <alexcho617@gmail.com>
2021-08-04 15:33:48 +0900

    widgetfied login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,6 +5,7 @@ import 'package:do_it_church/screens/find_id.dart';
 import 'package:flutter/material.dart';
 import 'notice_list.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:do_it_church/components/loginWidget.dart';
 
 //LOG IN SCREEN///////////////////////////////
 class LoginRoute extends StatefulWidget {
@@ -15,10 +16,6 @@ class LoginRoute extends StatefulWidget {
 }
 
 class _LoginRouteState extends State<LoginRoute> {
-  final _auth = FirebaseAuth.instance;
-  String userEmail = '';
-  String passWord = '';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,11 +24,6 @@ class _LoginRouteState extends State<LoginRoute> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              //Clip Oval로 이미지 적용시켜볼것
-              // ClipOval(
-              //   child: Image.asset('images/logo.png'),
-              // ),
-              //circle avatar도 좋으나 ListTile 과 주로 함께 프로필사진 쓰는용도임
               Hero(
                 tag: 'logo',
                 child: CircleAvatar(
@@ -43,120 +35,9 @@ class _LoginRouteState extends State<LoginRoute> {
               SizedBox(
                 height: 20,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 4),
-                    child: TextField(
-                      onChanged: (value1) => userEmail = value1,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '이메일',
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 4),
-                    child: TextField(
-                      onChanged: (value2) => passWord = value2,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '비밀번호',
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 4),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0))),
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Color(0xFF89A1F8)),
-                      ),
-                      child: Text(
-                        '로그인',
-                        style: kLogInButtonTextStyle,
-                      ),
-                      onPressed: () async {
-                        try {
-                          final newUser =
-                              await _auth.signInWithEmailAndPassword(
-                                  email: userEmail, password: passWord);
-
-                          if (newUser != null) {
-                            print('login success');
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => NoticeListRoute()),
-                            );
-                          }
-                        } catch (e) {
-                          print(e);
-                        }
-                        setState(() {
-                          //To change state here
-                        });
-                        print('User Email = $userEmail');
-                        print('Password = $passWord');
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HomeRoute()),
-                        );
-
-                      },
-                    ),
-                  ),
-                  //login button
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: TextButton(
-                          style: TextButton.styleFrom(primary: Colors.black54),
-                          child: Text('회원가입'),
-                          onPressed: () {
-                            setState(() {
-                              print('register button pressed');
-                            });
-
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => RegisterRoute()),
-                            );
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: TextButton(
-                          style: TextButton.styleFrom(primary: Colors.black54),
-                          child: Text('이메일/비밀번호 찾기'),
-                          onPressed: () {
-                            setState(() {
-                              print('find id/pw  button pressed');
-                            });
-
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => FindId()),
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+              LoginWidget(
+                textInput: '핸드폰번호',
+                buttonInput: '인증',
               ),
             ],
           ),
