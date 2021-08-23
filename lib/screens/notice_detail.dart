03ad73d5ddbcc8977f27fc9598f006885c7f463c
alexcho617 <alexcho617@gmail.com>
2021-08-23 15:26:13 +0900

    pre commit before comment listview

--- lib/screens/notice_detail.dart
+++ lib/screens/notice_detail.dart
@@ -1,10 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:do_it_church/components/comment.dart';
 import 'package:do_it_church/components/notice.dart';
 import 'package:do_it_church/screens/notice_new.dart';
 import 'package:do_it_church/constants.dart';
 import 'package:flutter/material.dart';
 import 'notice_new.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
+final _auth = FirebaseAuth.instance;
+final firestore = FirebaseFirestore.instance;
+
+Notice notice = Notice();
+Comment comment = Comment();
 
 class NoticeDetail extends StatefulWidget {
   NoticeDetail({required this.noticeId});
@@ -14,12 +21,7 @@ class NoticeDetail extends StatefulWidget {
 }
 
 class NoticeDetailState extends State<NoticeDetail> {
-  late TextEditingController _textEditingController = TextEditingController();
-
-  final _auth = FirebaseAuth.instance;
-  final firestore = FirebaseFirestore.instance;
-
-  Notice notice = Notice();
+  late TextEditingController commentTextController = TextEditingController();
 
   void getCurrentUser() async {
     try {
@@ -27,21 +29,57 @@ class NoticeDetailState extends State<NoticeDetail> {
       if (user != null) {
         User loggedInUser = user;
         print(
-            'SUCCESS(notice_detail_screen): Signed in As:${loggedInUser.phoneNumber}');
+            'SUCCESS(notice_detail_screen): Signed in As:${loggedInUser.uid}');
       }
     } catch (e) {
       print(e);
     }
   }
 
-  void getNoticeDetail() async {
+  void assignCurrentWriter() async {
+    final user = _auth.currentUser;
+    if (user != null) {
+      User loggedInUser = user;
+      print(
+          'SUCCESS(notice_new_screen): Signed in As:${loggedInUser.phoneNumber}');
+      comment.writer = loggedInUser.phoneNumber;
+      print('(notice_new_screen): Signed User UID:${loggedInUser.uid}');
+      QuerySnapshot userData = await FirebaseFirestore.instance
+          .collection('Users')
+          .where('uid', isEqualTo: loggedInUser.uid)
+          .get();
+      for (var doc in userData.docs) {
+        if (doc.exists) {
+          //print('Data:${doc.data()}');
+          print('(notice_detail): Signed User Name:${doc["name"]}');
+          comment.writer = doc["name"];
+        } else {
+          print('noData');
+        }
+      }
+    }
+  }
+
+  void getNoticeDetail(Notice notice) async {
     try {
-      firestore.collection("Notice").doc(widget.noticeId).get().then((DocumentSnapshot doc) {
-        notice.title = doc.get("title").toString();
-        notice.date = doc.get("date").toString();
-        notice.writer = doc.get("writer").toString();
-        notice.contents = doc.get("contents").toString();
-      });
+      //its missing the await
+      if (widget.noticeId != null) {
+        print('got noticeId');
+        await firestore
+            .collection("Notice")
+            .doc(widget.noticeId)
+            .get()
+            .then((DocumentSnapshot doc) {
+          notice.title = doc.get("title").toString();
+          notice.date = doc.get("date").toString();
+          notice.writer = doc.get("writer").toString();
+          notice.contents = doc.get("contents").toString();
+        });
+        print('${notice.title}');
+        print('${notice.writer}');
+        print('${notice.contents}');
+        print('fetched notice details');
+      }
     } catch (e) {
       print(e);
     }
@@ -51,12 +89,16 @@ class NoticeDetailState extends State<NoticeDetail> {
   void initState() {
     super.initState();
     getCurrentUser();
-    getNoticeDetail();
+    assignCurrentWriter();
     print(widget.noticeId);
   }
 
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      getNoticeDetail(notice);
+    });
+
     return MaterialApp(
       title: 'notice detail',
       home: Scaffold(
@@ -119,11 +161,10 @@ class NoticeDetailState extends State<NoticeDetail> {
                     children: [
                       Container(
                         child: NoticeDetailBuilder(
-                            title : notice.title,
-                            writer : notice.writer,
-                            date : notice.date,
-                            contents : notice.contents
-                        ),
+                            title: notice.title,
+                            writer: notice.writer,
+                            date: notice.date,
+                            contents: notice.contents),
                       ),
 
                       //구분선 만들기
@@ -140,46 +181,32 @@ class NoticeDetailState extends State<NoticeDetail> {
                           Expanded(
                             child: TextField(
                               //style:TextStyle(height:0.01, fontSize: 12),
-                              controller: _textEditingController,
+                              controller: commentTextController,
                               decoration: InputDecoration(hintText: "댓글 입력창"),
-                              onSubmitted: _handleSubmitted,
+                              //onSubmitted: _handleSubmitted(),
                             ),
                           ),
                           SizedBox(
                             width: 8.0,
                           ),
-                          FlatButton(
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              primary: Color(0xFF89A1F8),
+                            ),
                             onPressed: () {
-                              _handleSubmitted(_textEditingController.text);
-                              _textEditingController.clear();
+                              _handleSubmitted(
+                                  commentTextController.text, widget.noticeId);
+                              commentTextController.clear();
                             },
                             child: Text("완료"),
-                            color: Colors.blueAccent,
                           ),
                         ]),
                       ),
 
                       Container(
-                        color: Colors.white38,
-                        child: ListTile(
-                            //CircleAvatar() , use images from the images folder
-                            leading: CircleAvatar(
-                              backgroundImage: AssetImage(
-                                  'images/pro.jpg'), //always add images in directory
-                              maxRadius: 15,
-                            ),
-                            title: Text(
-                              '김은희(소망반)',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                              ),
-                            ),
-                            subtitle: Text(
-                              '확인완료했습니다! 이번에도 즐겁게!',
-                              style: TextStyle(fontSize: 13),
-                            )),
-                      ),
+                          // color: Colors.white38,
+                          // child: CommentBubble(),
+                          ),
                     ],
                   ),
                 ),
@@ -192,8 +219,40 @@ class NoticeDetailState extends State<NoticeDetail> {
   }
 }
 
+class CommentBubble extends StatelessWidget {
+  const CommentBubble({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        //CircleAvatar() , use images from the images folder
+        leading: CircleAvatar(
+          backgroundImage:
+              AssetImage('images/pro.jpg'), //always add images in directory
+          maxRadius: 15,
+        ),
+        title: Text(
+          '김은희(소망반)',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+        ),
+        subtitle: Text(
+          '확인완료했습니다! 이번에도 즐겁게!',
+          style: TextStyle(fontSize: 13),
+        ));
+  }
+}
+
 class NoticeDetailBuilder extends StatelessWidget {
-  const NoticeDetailBuilder({this.title, this.writer, this.date, this.contents});
+  const NoticeDetailBuilder(
+      {@required this.title,
+      @required this.writer,
+      @required this.date,
+      @required this.contents});
   final title;
   final writer;
   final date;
@@ -241,9 +300,9 @@ class NoticeDetailBuilder extends StatelessWidget {
                       },
                     ),
                     title: Text(
-                        '$title',
-                        style: kNoticeTitleTextStyle,
-                      ),
+                      '$title',
+                      style: kNoticeTitleTextStyle,
+                    ),
                     subtitle: Text(
                       '$writer',
                       style: kNoticeSubTitleTextStyle,
@@ -257,12 +316,10 @@ class NoticeDetailBuilder extends StatelessWidget {
             flex: 1,
             child: Container(
               padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                  '$contents',
+              child: Text('$contents',
                   softWrap: true,
                   overflow: TextOverflow.ellipsis,
-                  style: kNoticeContentTextStyle
-              ),
+                  style: kNoticeContentTextStyle),
             ),
           ),
         ],
@@ -271,8 +328,15 @@ class NoticeDetailBuilder extends StatelessWidget {
   }
 }
 
-void _handleSubmitted(String text) {
-  print(text);
+void _handleSubmitted(String commentText, String noticeId) async {
+  print(noticeId);
+  print(commentText);
+  print(comment.writer);
+  await firestore
+      .collection('Notice')
+      .doc(noticeId)
+      .collection('Comments')
+      .add({'comment': commentText, 'writer': comment.writer});
   //  _textEditingController.clear();
 }
 
@@ -312,89 +376,3 @@ void showAlertDialog(BuildContext context) async {
     },
   );
 }
-
-//Container(
-//   color: Colors.white38,
-//   child: ListTile(
-//       leading: CircleAvatar(
-//         backgroundImage: AssetImage(
-//             'images/pro1.png'), //always add images in directory
-//         maxRadius: 15,
-//       ),
-//       title: Text(
-//         '박세미(다윗반)',
-//         style: TextStyle(
-//           fontWeight: FontWeight.bold,
-//           fontSize: 15,
-//         ),
-//       ),
-//       subtitle: Text(
-//         '확인완료했습니다! 잘 준비합시다',
-//         style: TextStyle(fontSize: 13),
-//       )),
-// ),
-//
-// Container(
-//   color: Colors.white38,
-//   child: ListTile(
-//       leading: CircleAvatar(
-//         backgroundImage: AssetImage(
-//             'images/pro2.jpg'), //always add images in directory
-//         maxRadius: 15,
-//       ),
-//       title: Text(
-//         '고은미(사랑반)',
-//         style: TextStyle(
-//           fontWeight: FontWeight.bold,
-//           fontSize: 15,
-//         ),
-//       ),
-//       subtitle: Text(
-//         '확인완료했습니다! 늘 신경써주셔서 감사해요!',
-//         style: TextStyle(fontSize: 13),
-//       )),
-// ),
-//
-// Container(
-//   color: Colors.white38,
-//   child: ListTile(
-//       //CircleAvatar() , use images from the images folder
-//       leading: CircleAvatar(
-//         backgroundImage: AssetImage(
-//             'images/pro3.jpg'), //always add images in directory
-//         maxRadius: 15,
-//       ),
-//       title: Text(
-//         '김말희(믿음반)',
-//         style: TextStyle(
-//           fontWeight: FontWeight.bold,
-//           fontSize: 15,
-//         ),
-//       ),
-//       subtitle: Text(
-//         '확인완료~ 늘 수고가 많으세요:) 화이팅!',
-//         style: TextStyle(fontSize: 13),
-//       )),
-// ),
-//
-// Container(
-//   color: Colors.white38,
-//   child: ListTile(
-//       //CircleAvatar() , use images from the images folder
-//       leading: CircleAvatar(
-//         backgroundImage: AssetImage(
-//             'images/pro4.jpg'), //always add images in directory
-//         maxRadius: 15,
-//       ),
-//       title: Text(
-//         '박준기(기쁨반)',
-//         style: TextStyle(
-//           fontWeight: FontWeight.bold,
-//           fontSize: 15,
-//         ),
-//       ),
-//       subtitle: Text(
-//         '확인완료 항상 기도하고 있습니다!',
-//         style: TextStyle(fontSize: 13),
-//       )),
-// ),
