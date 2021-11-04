c264d0d88eb2d045cb0ced5660ae9cb1886d530a
alexcho617 <alexcho617@gmail.com>
2021-11-04 23:40:14 +0900

    added teacher model

---
+++ lib/components/teacher.dart
@@ -0,0 +1,20 @@
+class Teacher {
+  Teacher(
+      {required this.address,
+      required this.birthdate,
+      required this.department,
+      required this.familyInformation,
+      required this.name,
+      required this.phoneNumber,
+      required this.registrationDate,
+      required this.role});
+
+  final String address;
+  final String birthdate;
+  final String department;
+  final String familyInformation;
+  final String name;
+  final String phoneNumber;
+  final String registrationDate;
+  final String role;
+}
