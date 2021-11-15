7edea93f6a66dbe6bdbc7790c8dfee96f819c4f7
daun940203 <81723696+daun940203@users.noreply.github.com>
2021-11-15 21:20:47 +0900

    update

--- lib/components/MembersList.dart
+++ lib/components/MembersList.dart
@@ -1,20 +1,16 @@
-import 'package:do_it_church/components/customUser.dart';
 import 'package:do_it_church/components/teacher.dart';
 import 'package:do_it_church/components/teacherRepository.dart';
 import 'package:do_it_church/screens/member_info_detail.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'teacher.dart';
+import 'teacherRepository.dart';
 
-import 'ScreenDivider.dart';
+//교적관리 교사 리스트 화면입니다.
 
 class MembersList extends StatelessWidget {
   // const MembersList(this._customUser);
-  // final CustomUser _customUser;
-  Map<String, dynamic> data = {
-    "image": "images/pro.jpg",
-    "name": "고은미",
-    "categories": "교사",
-  };
+  //  final CustomUser _customUser;
 
   @override
   Widget build(BuildContext context) {
@@ -24,30 +20,37 @@ class MembersList extends StatelessWidget {
       print(teacher.name);
       print(teacher.address);
       print(teacher.phoneNumber);
+      print(teacher.department);
     }
 
-    return ListTile(
-      //contentPadding: EdgeInsets.symmetric(horizontal: 20),
-      leading: CircleAvatar(
-        backgroundImage:
-            AssetImage(data["image"]), //always add images in directory
-        radius: 20,
-      ),
-      title: Text(
-        data["name"],
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      //title: Text(_customUser.name),
-      //subtitle: Text("${_customUser.birthdate}"),
-      subtitle: Text(data["categories"]),
-      trailing: IconButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MemberInfoDetailRoute()),
-            );
-          },
-          icon: Icon(Icons.arrow_forward_ios)),
-    );
+
+      return ListTile(
+        //content Padding: EdgeInsets.symmetric(horizontal: 20),
+        leading: CircleAvatar(
+          backgroundImage:
+          AssetImage('images/pro.jpg'), //always add images in directory
+          radius: 20,
+        ),
+
+        // '${allTeacherInformation[0].name.toString()'}
+        // style: TextStyle(fontWeight: FontWeight.bold),
+
+        title: Text('${allTeacherInformation[0].name}',
+        style: TextStyle(fontWeight: FontWeight.bold)) ,
+
+        //title: Text(_customUser.name),
+        //subtitle: Text("${_customUser.birthdate}"),
+        subtitle: Text('${allTeacherInformation[0].role}'),
+        trailing: IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => MemberInfoDetailRoute()),
+              );
+            },
+            icon: Icon(Icons.arrow_forward_ios)),
+      );
+    }
   }
-}
+
